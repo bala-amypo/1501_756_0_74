09c090cf4f8e016b5bfb5d2856567daa49f7ca07
bala-amypo <bala@amypo.in>
2025-12-17 06:41:42 +0000

    User commit

--- src/main/java/com/example/demo/service/LocationServiceImpl.java
+++ src/main/java/com/example/demo/service/LocationServiceImpl.java
@@ -1,4 +1,9 @@
 package com.example.demo.service;
 public interface LoactionServiceImpl implements LocationService{
-
+   @Autowired
+   LocationRepository Locationrepo;
+   @Override
+   public LocationEntity createlocation(LocationEntity le){
+    return Locationrepo
+   }
 }
