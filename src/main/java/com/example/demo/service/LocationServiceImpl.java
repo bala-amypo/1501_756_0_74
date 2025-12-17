1e42ff6255f1f5b9a3e4f6d836b1887fd3c637e0
bala-amypo <bala@amypo.in>
2025-12-17 08:26:35 +0000

    User commit

--- src/main/java/com/example/demo/service/LocationServiceImpl.java
+++ src/main/java/com/example/demo/service/LocationServiceImpl.java
@@ -1,12 +1,26 @@
 package com.example.demo.service;
-public interface LoactionServiceImpl implements LocationService{
-   @Autowired
-   LocationRepository Locationrepo;
-   @Override
-   public LocationEntity createlocation(LocationEntity le){
-    return Locationrepo.save(le);
-   }
-   public List<LocationEntity> getall(){
-    return Locationrepo.findall();
-   }
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.LocationEntity;
+import com.example.demo.repository.LocationRepository;
+
+@Service
+public class LocationServiceImpl implements LocationService {
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+    @Override
+    public LocationEntity createlocation(LocationEntity le) {
+        return locationRepository.save(le);
+    }
+
+    @Override
+    public List<LocationEntity> getalllocation() {
+        return locationRepository.findAll();
+    }
 }
